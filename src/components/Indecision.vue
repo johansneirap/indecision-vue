9b5917cf111b92e5d2a5e4f533485b7e7dd5b33e
Johans Neira <77376519+johansneirap@users.noreply.github.com>
2021-08-17 15:17:35 -0400

    first commit

---
+++ src/components/Indecision.vue
@@ -0,0 +1,93 @@
+<template>
+  <img v-if="imgUrl" :src="imgUrl" alt="bg" >
+  <div class="bg-dark"></div>
+  <div class="indecision-container">
+      <input v-model="question" type="text" placeholder="Make me a question">
+      <p>Remember to end with a question mark (?)</p>
+      <div v-if="isValidQuestion">
+        <h2> {{ question }}</h2>
+        <h1>{{ answer }}</h1>
+      </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            question: null,
+            answer: null,
+            imgUrl: null,
+            isValidQuestion: false
+            }
+    },
+    methods: {
+        async getAnswer(){
+            this.answer = 'Thinking...'
+
+            const {answer, image} = await fetch('https://yesno.wtf/api').then( r => r.json() )
+            console.log(answer)
+            this.answer = answer
+            this.imgUrl = image
+        }
+    },
+    watch: {
+        question( value, oldValue ){
+            this.isValidQuestion = false 
+            if ( !value.includes('?') ) return
+
+            this.isValidQuestion = true 
+            this.getAnswer()
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+    img, .bg-dark {
+        height: 100vh;
+        left: 0px;
+        max-height: 100%;
+        max-width: 100%;
+        position: fixed;
+        top: 0px;
+        width: 100vw;
+    }
+
+    .bg-dark {
+        background-color: rgba(0, 0, 0, 0.4);
+    }
+
+    .indecision-container {
+        position: relative;
+        z-index: 99;
+    }
+    
+    input {
+        width: 250px;
+        padding: 10px 15px;
+        border-radius: 5px;
+        border: none;
+    }
+    input:focus {
+        outline: none;
+    }
+
+    p {
+        color: white;
+        font-size: 20px;
+        margin-top: 0px;
+    }
+
+    h1, h2 {
+        color: white;
+    }
+    
+    h2 {
+        margin-top: 150px;
+    }
+
+</style>
